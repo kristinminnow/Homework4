3f80b79f9e33df62032d28b291062a783dc59a0d
kristinminnow <kristin@mightyminnow.com>
2016-03-30 19:36:50 -0700

    shakespear homework 4

--- app.js
+++ app.js
@@ -5,19 +5,18 @@
 // Download the starter code.
 // Add a class of special to all of the <li> elements at the second level of the nested list.
 
-var myLIs = document.querySelectorAll('ul > li > ul > li');
+var myLIs = document.querySelectorAll('ul#selected-plays > li > ul > li');
 
 for (i = 0; i<myLIs.length; i++){
 	myLIs[i].classList.add('special');
 	};
 
-
 // Add a class of year to all of the table cells in the third column of a table.
 // Hint: Take a look at how many columns are in each table.
-var ThirdCol = document.querySelectorAll('table > tbody > tr > td:nth-child(3n-3)');
+var ThirdCol = document.querySelectorAll('table > tbody > tr > td:nth-child(3)');
 
 for (i = 0; i<ThirdCol.length; i++){
-	ThirdCol[i].classList.add('special');
+	ThirdCol[i].classList.add('year');
 	};
 // Make every other table row in both tables have a gray background.
 var OddRow = document.querySelectorAll('table > tbody > tr:nth-child(odd)');
